aac95a7fe2c13e3b2d7fc69978f17356048d7a18
Amber Frantz <amber.k.frantz@mgail.com>
2022-07-30 15:22:23 -0400

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,72 @@
+INSERT INTO bears (
+    name,
+    age,
+    sex,
+    color,
+    temperament,
+    alive
+) VALUES 
+(
+    'Mr. Chocolate',
+    5,
+    'F',
+    'Brown',
+    'Feisty',
+    TRUE
+),
+(
+    'Rowdy',
+    6,
+    'F',
+    'Brown',
+    'Mild',
+    TRUE
+),
+(
+    'Tabitha',
+    4,
+    'M',
+    'Brown',
+    'Sleepy',
+    TRUE
+),
+(
+    'Sergeant Brown',
+    2,
+    'M',
+    'Black',
+    'Grumpy',
+    TRUE
+),
+(
+    'Melissa',
+    9,
+    'F',
+    'Brown',
+    'Feisty',
+    FALSE
+),
+(
+    'Grinch',
+    4,
+    'F',
+    'Black',
+    'Grumpy',
+    TRUE
+),
+(
+    'Wendy',
+    10,
+    'M',
+    'Brown',
+    'Mild',
+    FALSE
+),
+(
+    NULL,
+    7,
+    'F',
+    'Brown',
+    'Feisty',
+    TRUE
+);
